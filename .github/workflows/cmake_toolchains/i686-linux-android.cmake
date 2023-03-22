81694d4d0a6c9be5427bcefea5f9195c16d161c2
Kartatz <105828205+Kartatz@users.noreply.github.com>
2023-03-22 02:20:44 -0300

    Refactoring to cmake toolchains

--- .github/workflows/cmake_toolchains/i686-linux-android.cmake
+++ .github/workflows/cmake_toolchains/i686-linux-android.cmake
@@ -4,3 +4,9 @@ set(ANDROID_NATIVE_API_LEVEL "21")
 set(TOOLCHAIN_PREFIX "i686-linux-android${ANDROID_NATIVE_API_LEVEL}")
 
 include("$ENV{ANDROID_HOME}/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_AR "${ANDROID_TOOLCHAIN_ROOT}/bin/llvm-ar" CACHE FILEPATH "ar")
+set(CMAKE_RANLIB "${ANDROID_TOOLCHAIN_ROOT}/bin/llvm-ranlib" CACHE FILEPATH "ranlib")
+set(CMAKE_STRIP "${ANDROID_TOOLCHAIN_ROOT}/bin/llvm-strip" CACHE FILEPATH "strip")
+set(CMAKE_OBJCOPY "${ANDROID_TOOLCHAIN_ROOT}/bin/llvm-objcopy" CACHE FILEPATH "objcopy")
+set(CMAKE_NM "${ANDROID_TOOLCHAIN_ROOT}/bin/llvm-nm" CACHE FILEPATH "nm")
